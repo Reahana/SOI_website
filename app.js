c0eccb6b961b409f818a4efb82cd1e99ca294958
Amanda-G10 <reahana233@gmail.com>
2021-07-25 13:36:32 +0600

    js

--- app.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  if (document.body.scrollTop >70 || document.documentElement.scrollTop >70) {
-    document.getElementsById("right").className = "slideUp";
-  
-  }
-}
